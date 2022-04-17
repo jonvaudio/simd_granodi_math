b0d200ce94c6b6a5c532a8073fc017c084b884e8
Jon Ville <100223577+jonvaudio@users.noreply.github.com>
2022-04-17 10:42:23 +0100

    latest simd_granodi.h, and forgot how to use templates

--- test_simd_granodi_math.cpp
+++ test_simd_granodi_math.cpp
@@ -25,38 +25,6 @@ void test_func(const int start, const int stop, const double scale,
     fclose(output);
 }
 
-// These are defined to get past problems with ambiguous overloads in the test
-// code
-inline Vec_ps log2_p3_ps(const Vec_ps& x) { return log2_p3(x); }
-inline Vec_f32x1 log2_p3_ss(const Vec_f32x1& x) { return log2_p3(x); }
-inline Vec_pd log2_p3_pd(const Vec_pd& x) { return log2_p3(x); }
-inline Vec_f64x1 log2_p3_sd(const Vec_f64x1& x) { return log2_p3(x); }
-
-inline Vec_ps exp2_p3_ps(const Vec_ps& x) { return exp2_p3(x); }
-inline Vec_f32x1 exp2_p3_ss(const Vec_f32x1& x) { return exp2_p3(x); }
-inline Vec_pd exp2_p3_pd(const Vec_pd& x) { return exp2_p3(x); }
-inline Vec_f64x1 exp2_p3_sd(const Vec_f64x1& x) { return exp2_p3(x); }
-
-inline Vec_ps logf_cm_ps(const Vec_ps& x) { return logf_cm(x); }
-inline Vec_f32x1 logf_cm_ss(const Vec_f32x1& x) { return logf_cm(x); }
-inline Vec_pd logf_cm_pd(const Vec_pd& x) { return logf_cm(x); }
-inline Vec_f64x1 logf_cm_sd(const Vec_f64x1& x) { return logf_cm(x); }
-
-inline Vec_ps expf_cm_ps(const Vec_ps& x) { return expf_cm(x); }
-inline Vec_f32x1 expf_cm_ss(const Vec_f32x1& x) { return expf_cm(x); }
-inline Vec_pd expf_cm_pd(const Vec_pd& x) { return expf_cm(x); }
-inline Vec_f64x1 expf_cm_sd(const Vec_f64x1& x) { return expf_cm(x); }
-
-inline Vec_ps sinf_cm_ps(const Vec_ps& x) { return sinf_cm(x); }
-inline Vec_f32x1 sinf_cm_ss(const Vec_f32x1& x) { return sinf_cm(x); }
-inline Vec_pd sinf_cm_pd(const Vec_pd& x) { return sinf_cm(x); }
-inline Vec_f64x1 sinf_cm_sd(const Vec_f64x1& x) { return sinf_cm(x); }
-
-inline Vec_ps cosf_cm_ps(const Vec_ps& x) { return cosf_cm(x); }
-inline Vec_f32x1 cosf_cm_ss(const Vec_f32x1& x) { return cosf_cm(x); }
-inline Vec_pd cosf_cm_pd(const Vec_pd& x) { return cosf_cm(x); }
-inline Vec_f64x1 cosf_cm_sd(const Vec_f64x1& x) { return cosf_cm(x); }
-
 int main() {
     std::string file_prefix;
     #ifdef SIMD_GRANODI_SSE2
@@ -72,22 +40,22 @@ int main() {
     #endif
 
     test_func(0, 20000, 0.01, file_prefix + "log2_p3_test",
-        log2_p3_ps, log2_p3_ss, log2_p3_pd, log2_p3_sd);
+        log2_p3<Vec_ps>, log2_p3<Vec_ss>, log2_p3<Vec_pd>, log2_p3<Vec_sd>);
 
     test_func(-2000, 2000, 0.01, file_prefix + "exp2_p3_test",
-        exp2_p3_ps, exp2_p3_ss, exp2_p3_pd, exp2_p3_sd);
+        exp2_p3<Vec_ps>, exp2_p3<Vec_ss>, exp2_p3<Vec_pd>, exp2_p3<Vec_sd>);
 
     test_func(0, 20000, 0.01, file_prefix + "logf_cm_test",
-        logf_cm_ps, logf_cm_ss, logf_cm_pd, logf_cm_sd);
+        logf_cm<Vec_ps>, logf_cm<Vec_ss>, logf_cm<Vec_pd>, logf_cm<Vec_sd>);
 
     test_func(-2000, 2000, 0.01, file_prefix + "expf_cm_test",
-        expf_cm_ps, expf_cm_ss, expf_cm_pd, expf_cm_sd);
+        expf_cm<Vec_ps>, expf_cm<Vec_ss>, expf_cm<Vec_pd>, expf_cm<Vec_sd>);
 
     test_func(-800, 800, 0.01, file_prefix + "sinf_cm_test",
-        sinf_cm_ps, sinf_cm_ss, sinf_cm_pd, sinf_cm_sd);
+        sinf_cm<Vec_ps>, sinf_cm<Vec_ss>, sinf_cm<Vec_pd>, sinf_cm<Vec_sd>);
 
     test_func(-800, 800, 0.01, file_prefix + "cosf_cm_test",
-        cosf_cm_ps, cosf_cm_ss, cosf_cm_pd, cosf_cm_sd);
+        cosf_cm<Vec_ps>, cosf_cm<Vec_ss>, cosf_cm<Vec_pd>, cosf_cm<Vec_sd>);
 
     return 0;
 }
